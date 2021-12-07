56488b717f13f8c69659d6e8e64c831792a878e5
qqiangwu <wqzhiep@gmail.com>
2021-12-07 22:55:29 +0800

    refactor(options): modify sst size to 16MB

--- include/cloudkv/options.h
+++ include/cloudkv/options.h
@@ -7,7 +7,7 @@ namespace cloudkv {
 struct options {
     bool open_only = false;
     std::uint64_t write_buffer_size = 4 * 1024 * 1024;
-    std::uint64_t sstable_size = 4 * 1024 * 1024;
+    std::uint64_t sstable_size = 16 * 1024 * 1024;
     std::uint64_t compaction_water_mark = 8;
 };
 
